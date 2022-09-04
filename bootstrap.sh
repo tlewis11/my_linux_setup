3dfe87be87f9413c4c9e30c4bd72325dc9aaaaf0
tlewis11 <tlewis11@github.com>
2022-09-04 00:48:37 -0500

    ansible playbook to place vimrc file

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+python3 -m pip install --user ansible
+ansible-playbook workstation.yml
+#apt-get install -y git vim docker.io
+#groupadd docker
+#usermod -aG docker tim
+#curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
--- bootstrap/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-apt-get install -y git vim docker.io
-groupadd docker
-usermod -aG docker tim
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
