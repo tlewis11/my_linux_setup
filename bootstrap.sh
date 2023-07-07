f8ab5d3b2d8978b1d930251982fd640781e237e7
tlewis11 <tlewis11@github.com>
2023-07-06 22:53:47 -0500

    add node tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-apt-get install nmap vim sshfs
-mv files/vimrc ~/.vimrc
+set -ex
+pip install --user ansible
+
+ansible-playbook dev-laptop.yaml
+
+sudo ln -s $(pwd)/node /usr/local/bin/node
+sudo ln -s $(pwd)/node /usr/local/bin/npm
+sudo ln -s $(pwd)/node /usr/local/bin/npx
+
+
+#apt-get install nmap vim sshfs
+#mv files/vimrc ~/.vimrc
