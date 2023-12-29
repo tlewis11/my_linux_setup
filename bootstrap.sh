7dba6db2db505079db4a6ca92962089af69dd1e7
tlewis11 <tlewis11@github.com>
2023-12-29 13:51:46 -0600

    simplify bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,3 @@ set -ex
 pip install --user ansible
 
 ansible-playbook dev-laptop.yaml
-
-sudo ln -s $(pwd)/node /usr/local/bin/node
-sudo ln -s $(pwd)/node /usr/local/bin/npm
-sudo ln -s $(pwd)/node /usr/local/bin/npx
-
-
-#apt-get install nmap vim sshfs
-#mv files/vimrc ~/.vimrc
